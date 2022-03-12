6e2a86377c7d79c75ba86a24237759b7dcafe03b
hugosenari <hugosenari@gmail.com>
2022-03-12 02:37:36 -0300

    chore: re index decimal

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''A correctly-rounded arbitrary precision decimal floating point arithmetic library'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."decimal-master".url = "path:./master";
+  inputs."decimal-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."decimal-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''A correctly-rounded arbitrary precision decimal floating point arithmetic library'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-decimal-master.flake = false;
+  inputs.src-decimal-master.owner = "inv2004";
+  inputs.src-decimal-master.ref   = "refs/heads/master";
+  inputs.src-decimal-master.repo  = "nim-decimal";
+  inputs.src-decimal-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-decimal-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-decimal-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
