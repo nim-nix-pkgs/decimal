c02fb9991de40d8fa6d97da6319f4564e8e66c7b
hugosenari <hugosenari@gmail.com>
2022-05-09 20:36:34 -0300

    chore: re index decimal

--- flake.nix
+++ flake.nix
@@ -7,14 +7,6 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs."decimal-master".dir   = "master";
-  inputs."decimal-master".owner = "nim-nix-pkgs";
-  inputs."decimal-master".ref   = "master";
-  inputs."decimal-master".repo  = "decimal";
-  inputs."decimal-master".type  = "github";
-  inputs."decimal-master".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."decimal-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
